fa675ae8b7fd00d0665b18cc49d8af9e7e608757
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-10-11 18:16:36 -0400

    commit tree 343e39849e3eda26d8a3cf5002601c3fba977519

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Samantha", streetAddress: "New York"
+};
+function updateEmployeeWithKeyAndValue(obj = employee, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj = employee, key, value) {
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj = employee, key) {
+    const newEmployee = { ...obj };
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(obj = employee, key) {
+    delete obj[key]
+    return obj
+}
